c4ffd60b8380f40d4bf0a3b963358995d84a872e
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-15 15:38:21 -0400

    Built login form component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login.js'
 
 function App() {
   return (
     <div className="App">
     <h1>Friends</h1>
+    <Login/>
     </div>
   );
 }
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,1 +1,53 @@
 import React from 'react';
+
+class Login extends React.Component {
+    state={
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+
+    handleChange = event => {
+this.setState({
+    credentials: {
+        ...this.state.credentials,
+        [event.target.name]: event.target.value
+    }
+})
+
+    }
+
+    render(){
+
+        return(
+            <div>
+
+                <form>
+            <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+            />
+            <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            />
+            <button>Log in</button>
+
+
+                </form>
+
+
+
+            </div>
+
+
+        );
+    }
+}
+
+export default Login;
