fe0b53aaf35b7951eb9430a9d3ec9fb62b0925a0
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-15 16:27:29 -0400

    Getting data in friendsList from console.log

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import {axiosWithAuth} from '../utils/axiosWithAuth.js'
 
 class FriendList extends React.Component {
     state= {
         friends: []
     };
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData =() => {
+
+        axiosWithAuth()
+        .get('/api/friends')
+        .then(response => console.log(response))
+    }
 
     render() {
 
